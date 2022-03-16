62ed47a16be972bc2258863e68b9a7cd934faa3f
tarunchauhan97 <chauhan.tarun897@gmail.com>
2022-03-16 09:56:50 +0530

    15 request & parse data

--- lib/app/services/api_service.dart
+++ lib/app/services/api_service.dart
@@ -24,4 +24,37 @@ class APIService {
         'Request ${api.tokenUri()} failed \n Response: ${response.statusCode} ${response.reasonPhrase}');
     throw response;
   }
+
+  Future<int> getEndpointData({
+    required String accessToken,
+    required EndPoint endPoint,
+  }) async {
+    final uri = api.endPointUri(endPoint);
+    final response = await http.get(
+      uri,
+      headers: {'Authorization': 'Bearer $accessToken'},
+    );
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      if (data.isNotEmpty) {
+        final Map<String, dynamic> endPointData = data[0];
+        final String? responseJsonKey = _responseJsonKeys[endPoint];
+        final int result = endPointData[responseJsonKey];
+        if (result != null) {
+          return result;
+        }
+      }
+    }
+    print(
+        'Request $uri failed \n Response:${response.statusCode} ${response.reasonPhrase}');
+    throw response;
+  }
+
+  static Map<EndPoint, String> _responseJsonKeys = {
+    EndPoint.cases: 'cases',
+    EndPoint.casesSuspected: 'data',
+    EndPoint.casesConfirmed: 'data',
+    EndPoint.deaths: 'data',
+    EndPoint.recovered: 'data',
+  };
 }
