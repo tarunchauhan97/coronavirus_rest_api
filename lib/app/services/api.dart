13a0c05cfeb0ae258577d6cc6b7cdcd7343a2341
tarunchauhan97 <chauhan.tarun897@gmail.com>
2022-03-16 09:31:56 +0530

    14 adding remaining endpoint

--- lib/app/services/api.dart
+++ lib/app/services/api.dart
@@ -1,6 +1,14 @@
 import 'package:coronavirus_rest_api_flutter_course/app/services/api_keys.dart';
 
-class API{
+enum EndPoint {
+  cases,
+  casesSuspected,
+  casesConfirmed,
+  deaths,
+  recovered,
+}
+
+class API {
   API({required this.apiKey});
   final String apiKey;
 
@@ -8,8 +16,23 @@ class API{
 
   static final String host = 'ncov2019-admin.firebaseapp.com';
 
-  Uri tokenUri() => Uri(scheme: 'https',host: host,path: 'token');
+  Uri tokenUri() => Uri(
+        scheme: 'https',
+        host: host,
+        path: 'token',
+      );
 
-  
+  Uri endPointUri(EndPoint endPoint) => Uri(
+        scheme: 'https',
+        host: host,
+        path: _paths[endPoint],
+      );
 
+  static Map<EndPoint, String> _paths = {
+    EndPoint.cases: 'cases',
+    EndPoint.casesSuspected: 'casesSuspected',
+    EndPoint.casesConfirmed: 'casesConfirmed',
+    EndPoint.deaths: 'deaths',
+    EndPoint.recovered: 'recovered',
+  };
 }
