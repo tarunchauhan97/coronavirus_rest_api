280bbc52acb2fb852e185f3f9b9d36c533226c74
tarunchauhan97 <chauhan.tarun897@gmail.com>
2022-03-19 13:58:52 +0530

    27 future.wait multiple requests

--- lib/app/repositories/data_repositories.dart
+++ lib/app/repositories/data_repositories.dart
@@ -6,21 +6,31 @@ class DataRepository {
   DataRepository({required this.apiService});
 
   final APIService apiService;
-  String? _accessToken;
+  String _accessToken = '';
 
   Future<int> getEndPointData(EndPoint endPoint) async {
     try {
       if (_accessToken == null) {
         _accessToken = await apiService.getAccessToken();
       }
-      return await apiService.getEndpointData(accessToken: _accessToken!, endPoint: endPoint);
+      return await apiService.getEndpointData(accessToken: _accessToken, endPoint: endPoint);
     } on Response catch (response) {
       if (response.statusCode == 401) {
         _accessToken = await apiService.getAccessToken();
-        return await apiService.getEndpointData(accessToken: _accessToken!, endPoint: endPoint);
+        return await apiService.getEndpointData(accessToken: _accessToken, endPoint: endPoint);
       }
       rethrow;
     }
+  }
+
+  Future<void> _getAllEndpointsData() async {
+    await Future.wait([
+      apiService.getEndpointData(accessToken: _accessToken, endPoint: EndPoint.cases),
+      apiService.getEndpointData(accessToken: _accessToken, endPoint: EndPoint.casesSuspected),
+      apiService.getEndpointData(accessToken: _accessToken, endPoint: EndPoint.casesConfirmed),
+      apiService.getEndpointData(accessToken: _accessToken, endPoint: EndPoint.deaths),
+      apiService.getEndpointData(accessToken: _accessToken, endPoint: EndPoint.recovered),
+    ]);
   }
 
 // Future<int> getEndPointData(EndPoint endPoint) async {
