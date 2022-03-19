055cae5281f81197fd91af0ec12f11a546c7a431
tarunchauhan97 <chauhan.tarun897@gmail.com>
2022-03-19 12:05:59 +0530

    20 loading & refreshing tokem

--- lib/app/repositories/data_repositories.dart
+++ lib/app/repositories/data_repositories.dart
@@ -1,13 +1,30 @@
 import 'package:coronavirus_rest_api_flutter_course/app/services/api.dart';
 import 'package:coronavirus_rest_api_flutter_course/app/services/api_service.dart';
+import 'package:http/http.dart';
 
 class DataRepository {
   DataRepository({required this.apiService});
 
   final APIService apiService;
+  String? _accessToken;
 
   Future<int> getEndPointData(EndPoint endPoint) async {
-    final accessToken = await apiService.getAccessToken();
-    return await apiService.getEndpointData(accessToken: accessToken, endPoint: endPoint);
+    try {
+      if (_accessToken == null) {
+        _accessToken = await apiService.getAccessToken();
+      }
+      return await apiService.getEndpointData(accessToken: _accessToken!, endPoint: endPoint);
+    } on Response catch (response) {
+      if (response.statusCode == 401) {
+        _accessToken = await apiService.getAccessToken();
+        return await apiService.getEndpointData(accessToken: _accessToken!, endPoint: endPoint);
+      }
+      rethrow;
+    }
   }
+
+// Future<int> getEndPointData(EndPoint endPoint) async {
+//   final accessToken = await apiService.getAccessToken();
+//   return await apiService.getEndpointData(accessToken: accessToken, endPoint: endPoint);
+// }
 }
