ab883fad3f101e1339dc277c7b245f304b033cd6
tarunchauhan97 <chauhan.tarun897@gmail.com>
2022-03-19 12:32:04 +0530

    21 adding provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:coronavirus_rest_api_flutter_course/app/repositories/data_repositories.dart';
 import 'package:coronavirus_rest_api_flutter_course/app/services/api.dart';
 import 'package:coronavirus_rest_api_flutter_course/app/services/api_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,12 +14,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Provider<DataRepository>(
+      create: (_) => DataRepository(apiService: APIService(API.sandbox())),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Coronavirus Tracker',
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: Color(0xff101010),
+          cardColor: Color(0xff222222),
+        ),
+        home: const MyHomePage(title: 'Flutter Demo Home Page'),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
